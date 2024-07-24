c5ab60a49fa9fbeb651f02fc46a370ec97e7c43d
wenhuyu <wsy2161826815@163.com>
2024-07-24 21:02:15 +0800

    格式化修改脚本注释

--- Plugin/clang-format/format_incremental.sh
+++ Plugin/clang-format/format_incremental.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
-# Get the directory of the current script
+# 获取当前脚本的目录
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
-# Set the path to clang-format and the configuration file
+# 设置clang-format和配置文件的路径
 CLANG_FORMAT_PATH="$SCRIPT_DIR/clang-format"
 CONFIG_PATH="$SCRIPT_DIR/.clang-format"
 
-# Root directory is relative to the current directory ../../
+# 根目录相对于当前目录 ../../
 ROOT_DIR=$(realpath "$SCRIPT_DIR/../../")
 
-
-# Set allowed folders, based on root directory
+# 设置允许的文件夹，基于根目录
 FOLDERS=("$ROOT_DIR/Component" "$ROOT_DIR/HAL" "$ROOT_DIR/Project" "$ROOT_DIR/Protocol" "$ROOT_DIR/Utilities")
 
-# Add all subfolders containing "Board" under Platform
+# 添加Platform目录下所有包含“Board”的子文件夹
 for d in "$ROOT_DIR/Platform/"*/ ; do
     if [ -d "$d/Board" ]; then
         FOLDERS+=("$d/Board")
     fi
 done
 
-# Get the list of staged files
+# 获取暂存的文件列表
 CHANGED_FILES=($(git diff --name-only --cached --diff-filter=ACM | grep -E "\.(cpp|hpp|c|h)$"))
 
 if [ ${#CHANGED_FILES[@]} -eq 0 ]; then
     exit 0
 fi
 
-# Iterate over each staged file and format if necessary
+# 遍历每个暂存的文件，并在必要时格式化
 for FILE_PATH in "${CHANGED_FILES[@]}"; do
     FILE_PATH_ABS="$ROOT_DIR/$FILE_PATH"
 
-    # Check if the file is in allowed folders
+    # 检查文件是否在允许的文件夹中
     FORMAT_FILE=0
     for FOLDER in "${FOLDERS[@]}"; do
         if [[ "$FILE_PATH_ABS" == "$FOLDER"* ]]; then
@@ -41,14 +40,14 @@ for FILE_PATH in "${CHANGED_FILES[@]}"; do
         fi
     done
 
-    # Format the file if it is in an allowed folder and not in build or release directories
+    # 如果文件在允许的文件夹中且不在build或release目录中，则格式化文件
     if [ $FORMAT_FILE -eq 1 ] && [[ "$FILE_PATH" != *"build/"* ]] && [[ "$FILE_PATH" != *"release/"* ]]; then
-        echo "Formatting file: $FILE_PATH"
+        echo "格式化文件: $FILE_PATH"
         "$CLANG_FORMAT_PATH" -style=file -i "$FILE_PATH"
         if [ $? -ne 0 ]; then
-            echo "Failed to format $FILE_PATH"
+            echo "格式化 $FILE_PATH 失败"
         fi
     fi
 done
 
-echo "Incremental formatting complete."
+echo "增量格式化完成。"
